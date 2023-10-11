7e006c236699e36e7406e1c483865cd1df2570cd
laboratorio <laboratorio@UPOAULA10507>
2023-10-11 11:06:02 -0500

    Agregar archivos de proyecto.

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using CRUDMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestAPI.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+
+        public ApplicationDBContext(
+            DbContextOptions<ApplicationDBContext> options
+            ) : base(options) { }
+
+            DbSet<Producto> Producto { get; set; }
+
+
+    }
+}
